94d85cbb3015e7ba0f922a5e8cf6f7b33adfabb0
timur0907 <timurabdulvohidov@gmail.com>
2022-04-04 20:53:47 +0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// pre_img sccript
+let pre_img = document.getElementById('pre_img'); 
+
+function add_class_delay() {  
+    pre_img.classList.remove('pre_img');
+    pre_img.classList.add('delay');
+    document.querySelector('.body').classList.remove('active');
+}
+
+setTimeout(add_class_delay, 2000);
+
+// pre_img script end
+
+
+// projects slider
+
+let projectSlideIndex = 0;
+showProjectsSlides();
+
+function showProjectsSlides() {
+  let i;
+  let slidesProject = document.getElementsByClassName("slide");
+  for (i = 0; i < slidesProject.length; i++) {
+    slidesProject[i].style.display = "none";
+  }
+  projectSlideIndex++;
+  if (projectSlideIndex > slidesProject.length) {projectSlideIndex = 1}
+  slidesProject[projectSlideIndex-1].style.display = "block";
+  setTimeout(showProjectsSlides, 2000); // Change image every 2 seconds
+}
